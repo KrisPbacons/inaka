99aa49cfeea521a9cd7ac96184b8df2641b307ef
KrisPbacons <sidparsan@ymail.com>
2026-06-18 21:53:40 +0900

    Added growing plants, Onions and carrots and the ability to pick them

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,3 +1,7 @@
 extends Node
 
 var weather
+
+var plantselected = 1 #1 = carrot, 2 = onion
+var numofcarrots = 0
+var numofonions = 0
---
+++ Scripts/growingzone.gd
@@ -0,0 +1,60 @@
+extends StaticBody2D
+
+var plant = global.plantselected
+var plantgrowing = false
+var plant_grown = false
+
+func _physics_process(delta: float) -> void:
+	if !plantgrowing:
+		plant = global.plantselected
+		
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if !plantgrowing:
+		if plant == 1:
+			plantgrowing = true
+			$carrotgrowtimer.start()
+			$plant.play("carrotgrowing")
+		if plant == 2:
+			plantgrowing = true
+			$oniongrowtimer.start()
+			$plant.play("oniongrowing")
+		else:
+			print("plant already growing")
+			
+func _on_carrotgrowtimer_timeout() -> void:
+	var carrot_plant = $plant
+	if carrot_plant.frame == 0:
+		carrot_plant.frame = 1
+		$carrotgrowtimer.start()
+	elif carrot_plant.frame == 1:
+		carrot_plant.frame = 2
+		plant_grown = true
+
+func _on_oniongrowtimer_timeout() -> void:
+	var onion_plant = $plant
+	if onion_plant.frame == 0:
+		onion_plant.frame = 1
+		$oniongrowtimer.start()
+	elif onion_plant.frame == 1:
+		onion_plant.frame = 2
+		plant_grown = true
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("click"):
+		if plant_grown == true:
+			if plant == 1:
+				global.numofcarrots += 1
+				plantgrowing = false
+				plant_grown = false
+				$plant.play("none")
+			elif plant == 2:
+				global.numofonions += 1
+				plantgrowing = false
+				plant_grown = false
+				$plant.play("none")
+			else:
+				pass
+		print(global.numofcarrots)
+		print(global.numofonions)
